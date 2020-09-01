84ba89bfec86850bc1871e9a595d13df43cc8dfb
Thanh Nguyen <thanhnguyen2187@gmail.com>
2020-09-01 15:48:09 +0700

    Add Windscribe

---
+++ windscribe/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key FDC247B7
+echo 'deb https://repo.windscribe.com/ubuntu bionic main' | sudo tee /etc/apt/sources.list.d/windscribe-repo.list
+
+sudo apt install windscribe-cli
+
+windscribe login
+windscribe connect
