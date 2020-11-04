c3d1a21f9608d84216fb6aa6ecf12cc708e4dee9
Thanh Nguyen <thanhnguyen2187@gmail.com>
2020-11-04 20:47:07 +0700

    Change Vim's plugin manager to vim plug with NeoVim

--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo apt install vim
+sudo apt install neovim
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
