58b020b12f626f5dfe146b4814b7fffc415e525d
Thanh Nguyen <thanhnguyen2187@gmail.com>
2020-12-28 23:19:10 +0700

    Add update command of apt to smooth the installing

--- windscribe/install.sh
+++ windscribe/install.sh
@@ -2,6 +2,7 @@
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key FDC247B7
 echo 'deb https://repo.windscribe.com/ubuntu bionic main' | sudo tee /etc/apt/sources.list.d/windscribe-repo.list
+sudo apt update
 
 sudo apt install windscribe-cli
 
