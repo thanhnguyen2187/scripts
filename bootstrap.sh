457e825b3bd64d6d89ff30a96a186babb5d637b0
Thanh Nguyen <thanhnguyen2187@gmail.com>
2021-06-26 09:07:18 +0700

    Fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo apt install git
+sudo apt install \
+    git \
+    build-essential \
+;
 git clone https://github.com/thanhnguyen2187/scripts
 git clone https://github.com/thanhnguyen2187/.dotfiles ~/.dotfiles
 cd scripts || exit
