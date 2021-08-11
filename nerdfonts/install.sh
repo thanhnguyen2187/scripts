40179acb2523aee57b9a29eabe6981658ee8eac6
Thanh Nguyen <thanhnguyen2187@gmail.com>
2021-08-11 21:15:58 +0700

    Add -o to NerdFonts script's curl

--- nerdfonts/install.sh
+++ nerdfonts/install.sh
@@ -4,7 +4,7 @@ FONTNAME=$1
 VERSION=$2
 
 rm -f "/tmp/${FONTNAME}.zip"
-curl "https://github.com/ryanoasis/nerd-fonts/releases/download/v${VERSION}/${FONTNAME}.zip" -LOJ
+curl -LJ "https://github.com/ryanoasis/nerd-fonts/releases/download/v${VERSION}/${FONTNAME}.zip" -o "/tmp/${FONTNAME}.zip"
 unzip "/tmp/${FONTNAME}.zip" -d /tmp/fonts
 mkdir -p ~/.local/share/fonts
 cp /tmp/fonts/* ~/.local/share/fonts/
