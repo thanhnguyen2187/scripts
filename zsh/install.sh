6c421a54435308c126a7fef91889cc1b3fc22625
Thanh Nguyen <thanhnguyen2187@gmail.com>
2021-08-11 21:16:19 +0700

    Fix zsh's install script ordering

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,8 +6,10 @@ chsh -s "$(command -v zsh)"
 
 # install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+rm -rf "${HOME}/.oh-my-zsh/custom"
+
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-rm -rf "${HOME}/.oh-my-zsh/custom"
+
 ln -s "${HOME}/.dotfiles/.oh-my-zsh/custom" "${HOME}/.oh-my-zsh/custom"
