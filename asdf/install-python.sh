420b6c6fb4eb33d51df22eaeade4b1b68897b7d8
Thanh Nguyen <thanhnguyen2187@gmail.com>
2021-10-11 12:40:13 +0700

    Add Python installation to asdf

---
+++ asdf/install-python.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt install \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    curl \
+    llvm \
+    libncurses5-dev \
+    libncursesw5-dev \
+    xz-utils \
+    tk-dev \
+    libffi-dev \
+    liblzma-dev \
+    -y \
+;
+asdf plugin add python
