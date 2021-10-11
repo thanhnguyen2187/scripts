8c3c04c61c48c126a3b051b052f005042dd566c7
Thanh Nguyen <thanhnguyen2187@gmail.com>
2021-10-11 12:40:00 +0700

    Fix NodeJS installation of asdf

--- asdf/install-nodejs.sh
+++ asdf/install-nodejs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 sudo apt install gpg dirmngr
+asdf plugin add nodejs
+
 bash -c "${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring"
+
 asdf install nodejs latest:lts
 asdf global nodejs lts
