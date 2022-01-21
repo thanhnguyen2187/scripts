e24ccf02b7e6b7ff4987e3d378f89bcf1636c3f6
Thanh Nguyen <thanhnguyen2187@gmail.com>
2022-01-21 21:01:38 +0700

    Tidy Vim

--- vim/install.sh
+++ vim/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo apt install vim
-sudo apt install neovim
+# sudo apt install neovim
 
 # curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 #     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # vim +PlugInstall +qall
-nvim --headless +PlugInstall +UpdateRemotePlugins +qa
+# nvim --headless +PlugInstall +UpdateRemotePlugins +qa
